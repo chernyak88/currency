6c8ffefe1f07835c42ba73d76d85ff1551a55f99
Igor Chernyak <i_chernyak@inbox.ru>
2020-10-13 14:16:47 +0300

    Updates

---
+++ precache-manifest.186ea463b325a6aedbef62617f066197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a299bdeefa909869bcbf893a21797e0",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "db10ad9b30b067c2eca4",
+    "url": "/currency/static/css/main.8ef719d5.chunk.css"
+  },
+  {
+    "revision": "0addb056e70532d9b475",
+    "url": "/currency/static/js/2.e017b4ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency/static/js/2.e017b4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db10ad9b30b067c2eca4",
+    "url": "/currency/static/js/main.40f89df6.chunk.js"
+  },
+  {
+    "revision": "52fa3b2951c2fae3bb02",
+    "url": "/currency/static/js/runtime-main.1c7c7791.js"
+  },
+  {
+    "revision": "209610ddfca73822d1c29bc9773ed419",
+    "url": "/currency/static/media/bg.209610dd.jpg"
+  }
+]);
